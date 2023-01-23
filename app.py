c0d1f052002dc4252be6f9438b55a5ea395de6d7
Nuraiym Obozkanova <nuraika.obozkanova@gmail.com>
2023-01-23 22:02:11 +0600

    tel bot is now working

--- app.py
+++ app.py
@@ -10,18 +10,30 @@ from middleware.dbConnect import DBConnect
 
 dotenv.load_dotenv()
 
-TOKEN=os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN')
 
 debug_mode = True if os.getenv('DEBUG_MODE') == '1' else False
 
 if debug_mode:
-    logging.basicConfig(filename='scraper.log', level=logging.DEBUG, format="%(asctime)s - %(message)s",
-                        datefmt="%d-%b-%y %H:%M:%S")
+    logging.basicConfig(filename='scraper.log', level=logging.DEBUG, format="{asctime} {levelname:<8} {message}",
+                        style="{", datefmt="%d-%b-%y %H:%M:%S", filemode="w")
 
 
 app = Flask(__name__)
 app.wsgi_app = DBConnect(app.wsgi_app)
 
+
+def get_file_path():
+    return '/Users/nuraim/Desktop/Cover_letter.docx'
+
+
+def get_log_data():
+    try:
+        with open("scraper.log", "r") as f:
+            log_data = f.read()
+    except:
+        log_data = "Error reading log file"
+    return log_data
 
 def parse_message(message):
     print("message -->", message)
@@ -30,6 +42,8 @@ def parse_message(message):
         txt = message["message"]["text"]
         print("chat_id-->", chat_id)
         print("txt-->", txt)
+        if not chat_id:
+            raise ValueError("Invalid chat_id")
         return chat_id, txt
     except:
         print('No text found-->>')
@@ -45,18 +59,33 @@ def tel_send_message(chat_id, text):
     return k
 
 
+def tel_send_log(chat_id, max_length = 1000):
+    try:
+        with open("scraper.log", "r") as f:
+            log_data = f.read()
+            log_data = log_data[-max_length:]
+    except:
+        log_data = "Error reading log file"
+    tel_send_message(chat_id, log_data)
+
+
+
 def tel_send_document(chat_id):
+    file_path = get_file_path()
     url = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
-    # document = open('/Users/nuraim/Desktop/Cover_letter.docx', 'rb')
+    file = {'document': open(file_path, 'rb')}
     payload = {
         "chat_id": chat_id,
-        "document": "/Users/nuraim/Desktop/reference.pdf"
     }
-    k = requests.post(url, json=payload)
-    return k
+    r = requests.post(url, json=payload, files=file)
+    if r.status_code != 200:
+        print(r.content)
+    else:
+        print(r.json())
 
 
 app.add_url_rule('/logging', view_func=FlaskLogging.get, endpoint='logging_get', methods=['GET'])
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -68,6 +97,8 @@ def index():
             tel_send_message(chat_id, "Hello!")
         elif txt == "doc":
             tel_send_document(chat_id)
+        elif txt == "log":
+            tel_send_log(chat_id)
         else:
             tel_send_message(chat_id, "Hello, World!")
 
@@ -77,4 +108,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', '5000', debug=debug_mode, threaded=True)
+    app.run('0.0.0.0', '5000', debug=True, threaded=True)
